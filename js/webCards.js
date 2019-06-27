30a2863f5c00f54d21a96207451430d03c4bbd2d
xwaxes01 <xwaxes@gmail.com>
2019-06-27 21:47:02 +0300

    Minor changes to make the navbar responsive on scroll

--- js/webCards.js
+++ js/webCards.js
@@ -1,3 +1,16 @@
+// When the user scrolls down 50px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("navbar").style.padding = "0px";
+    } else {
+        document.getElementById("navbar").style.padding = "55px 0px";
+    }
+}
+
 // Add active class to the current button (highlight it)
 var header = document.getElementById("navbar-links");
 var btns = header.getElementsByClassName("navbar-links-item");
